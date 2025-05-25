26fe89875cf7520d486a847021d6641832faaf78
Kristers Jānītis 221RDB301 <118080877+221RDB301@users.noreply.github.com>
2025-05-25 23:07:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,17 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-  'index.html',
-  'style.css',
-  'app.js',
-  'db.js',
-  'manifest.json'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/db.js',
+  '/manifest.json',
 ];
 
 // Instalēšana
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return Promise.all(
-        urlsToCache.map((url) => {
-          return fetch(url).then((response) => {
-            if (!response.ok) {
-              throw new Error(`Request for ${url} failed with status ${response.status}`);
-            }
-            return cache.put(url, response.clone());
-          });
-        })
-      );
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
