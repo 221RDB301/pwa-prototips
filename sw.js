2315cb618425748c9ccc06c793f538d73357e51f
Kristers Jānītis 221RDB301 <118080877+221RDB301@users.noreply.github.com>
2025-05-25 23:03:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,18 @@ const urlsToCache = [
 // Instalēšana
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+  return Promise.all(
+    urlsToCache.map((url) => {
+      return fetch(url).then((response) => {
+        if (!response.ok) {
+          throw new Error(`Request for ${url} failed with status ${response.status}`);
+        }
+        return cache.put(url, response.clone());
+      });
+    })
+  );
+});
   );
 });
 
