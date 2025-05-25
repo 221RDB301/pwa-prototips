d68d153d7cb0f2a5f832d9c9cb57133f8e960997
Kristers Jānītis 221RDB301 <118080877+221RDB301@users.noreply.github.com>
2025-05-25 23:06:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,17 +11,17 @@ const urlsToCache = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-  return Promise.all(
-    urlsToCache.map((url) => {
-      return fetch(url).then((response) => {
-        if (!response.ok) {
-          throw new Error(`Request for ${url} failed with status ${response.status}`);
-        }
-        return cache.put(url, response.clone());
-      });
+      return Promise.all(
+        urlsToCache.map((url) => {
+          return fetch(url).then((response) => {
+            if (!response.ok) {
+              throw new Error(`Request for ${url} failed with status ${response.status}`);
+            }
+            return cache.put(url, response.clone());
+          });
+        })
+      );
     })
-  );
-});
   );
 });
 
